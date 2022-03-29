13883baf2f7a3e8f5fd92e79747b2c7b3c92e3d5
Kayson Fu <kayson.fu@crypto.com>
2022-03-29 14:22:51 +0800

    added clean up

--- test/federated.test.tsx
+++ test/federated.test.tsx
@@ -9,20 +9,56 @@ global.fetch = jest.fn((s) =>
 );
 
 describe("Federation", function () {
-  it("Testing CompInSrc from Remote", async function () {
+  // it("Testing CompInSrc from Remote", async function () {
+  //   const React = await import("react");
+  //   const { render, screen, waitFor } = await import("@testing-library/react");
+  //   // const { shallow, mount, render } = await import("enzyme");
+
+  //   const Comp = (await CompInSrc).default;
+  //   render(<Comp />);
+
+  //   expect(screen.getByText("CompInSrc component")).toBeTruthy();
+
+  //   await waitFor(() => {
+  //     // expect(global.fetch).toBeCalledWith("g");
+  //     expect(screen.getByText(`status is: {"status":123}`)).toBeTruthy();
+  //   });
+  // });
+
+  it("Testing CompInSrc2 from Remote", async function () {
     const React = await import("react");
-    const { render, screen, waitFor } = await import("@testing-library/react");
+    const { render, waitFor, cleanup } = await import("@testing-library/react");
     // const { shallow, mount, render } = await import("enzyme");
 
     const Comp = (await CompInSrc).default;
-    render(<Comp />);
+    const { getByText } = render(<Comp />);
 
-    expect(screen.getByText("CompInSrc component")).toBeTruthy();
+    expect(getByText("CompInSrc component")).toBeTruthy();
 
     await waitFor(() => {
       // expect(global.fetch).toBeCalledWith("g");
-      expect(screen.getByText(`status is: {"status":123}`)).toBeTruthy();
+      expect(getByText(`status is: {"status":123}`)).toBeTruthy();
     });
+
+    cleanup();
+  });
+
+  it("Testing CompInSrc from Remote", async function () {
+    const React = await import("react");
+    const { render, waitFor, cleanup } = await import("@testing-library/react");
+    // const { shallow, mount, render } = await import("enzyme");
+
+    const Comp = (await CompInSrc).default;
+    const { getByText } = render(<Comp />);
+
+    expect(getByText("CompInSrc component")).toBeTruthy();
+
+    await waitFor(() => {
+      // expect(global.fetch).toBeCalledWith("g");
+      expect(getByText(`status is: {"status":123}`)).toBeTruthy();
+    });
+
+    cleanup();
   });
 
   // it("Testing CompInSrc from OptionComp", async function () {
